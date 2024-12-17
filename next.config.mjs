dc2e56276f071635427f5e74bc30c2080ea6ba53
Mahdi Zeraati <mahdizeraatidev@gmail.com>
2024-12-18 01:58:51 +0330

    dep test

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    assetPrefix : '/',
+    // output: 'export',
+    // assetPrefix : '/',
 };
 export default nextConfig;
